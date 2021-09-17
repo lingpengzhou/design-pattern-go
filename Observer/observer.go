f51bd96a58b55af747f21ff9cbcfa6df8b5daf10
zhoulingpeng <xiaoer412@yahoo.com>
2021-09-17 17:35:36 +0800

    first version

--- Observer/observer.go
+++ Observer/observer.go
@@ -1,9 +1,53 @@
 package Observer
 
+import "log"
+
 type Observer interface {
-	update()
+	update(state int)
 }
 
-type BinaryObserver struct {
+type Subject struct {
+	observers []Observer
+	state     int
+}
 
+func (subject *Subject) getState() int {
+	return subject.state
+}
+
+func (subject *Subject) setState(state int) {
+	subject.state = state
+	subject.notifyAll()
+}
+
+func (subject *Subject) attach(observer Observer) {
+	subject.observers = make([]Observer, 0)
+	subject.observers = append(subject.observers, observer)
+}
+
+func (subject *Subject) notifyAll() {
+	for _, val := range subject.observers {
+		val.update(subject.state)
+	}
+}
+
+type OriginObserver struct {
+}
+
+func (originObserver *OriginObserver) update(state int) {
+	log.Println("originObserver:", state)
+}
+
+type PlusObserver struct {
+}
+
+func (plusObserver *PlusObserver) update(state int) {
+	log.Println("plusObserver:", state+1)
+}
+
+type DecreaseObserver struct {
+}
+
+func (decreaseObserver *DecreaseObserver) update(state int) {
+	log.Println("decreaseObserver:", state-1)
 }
